3bd0dd550431b1d0ec2d57ede0eda76c09a60912
Shyam Bista <shyam.bista3210@gmail.com>
2025-08-25 09:29:18 +0545

    Remove token after expiration

--- src/app/interceptors/auth-interceptor.ts
+++ src/app/interceptors/auth-interceptor.ts
@@ -1,6 +1,7 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth-service';
+import { catchError, throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
 
@@ -14,5 +15,13 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
       }
     })
   }
-  return next(req);
+  return next(req).pipe(
+    catchError((error: any)=>{
+      if(error instanceof HttpErrorResponse && error.status == 403){
+        console.log('Token expired or invalid');
+        authService.logout();
+      }
+      return throwError(()=> error);
+    })
+  );
 };
